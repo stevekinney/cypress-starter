84c59d59fa421688a834d3e92325c1c3d80501c5
Steve Kinney <hello@stevekinney.net>
2021-12-17 08:28:08 -0600

    Update Application.js

--- src/Application.js
+++ src/Application.js
@@ -1,7 +1,9 @@
 function Application() {
   return (
-    <main class="flex justify-center items-center h-screen">
-      <h1 class="text-3xl font-extrabold">The world's simplest application.</h1>
+    <main className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-extrabold">
+        The world's simplest application.
+      </h1>
     </main>
   );
 }
