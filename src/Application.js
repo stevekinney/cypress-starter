484d5b66a37228f77208410e60784d81d4b117fc
Steve Kinney <hello@stevekinney.net>
2021-12-17 06:12:05 -0600

    Just make it simple

--- src/Application.js
+++ src/Application.js
@@ -1,26 +1,8 @@
-import { Routes, Route, Link } from 'react-router-dom';
-
 function Application() {
   return (
-    <div className="grid grid-cols-12 grid-rows-2">
-      <h1 className="text-3xl font-bold bg-red-300 col-span-12">
-        Hello world!
-      </h1>
-      <nav className="grid-cols-3 flex flex-rows">
-        <article>
-          <Link to="/">Home</Link>
-        </article>
-        <article>
-          <Link to="/about">About</Link>
-        </article>
-      </nav>
-      <section className="grid-cols-9">
-        <Routes>
-          <Route path="/" element={<p>Home</p>} />
-          <Route path="about" element={<p>About</p>} />
-        </Routes>
-      </section>
-    </div>
+    <main class="flex justify-center items-center h-screen">
+      <h1 class="text-3xl font-extrabold">The world's simplest application.</h1>
+    </main>
   );
 }
 
